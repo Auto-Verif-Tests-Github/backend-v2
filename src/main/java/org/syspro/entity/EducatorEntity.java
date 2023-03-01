6efc557bb99415e81acd07a07e41c4d5647c063b
Eugene <genyamateyuk@gmail.com>
2023-03-02 05:11:31 +0700

    generated strategy

--- src/main/java/org/syspro/entity/EducatorEntity.java
+++ src/main/java/org/syspro/entity/EducatorEntity.java
@@ -1,11 +1,14 @@
 package org.syspro.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "educators")
 public class EducatorEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String login, password, fullName;
 
